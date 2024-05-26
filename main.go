b1454b814916aa5d3f2a32345f7f43aab7a48eff
Tauan Camargo <lukyhenson@gmail.com>
2024-05-26 06:23:07 -0300

    feat: consume filemanager constructor and pass to price

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
 
@@ -8,7 +11,8 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		priceJob := prices.New(taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.New(fm, taxRate)
 		priceJob.Process()
 	}
 }
