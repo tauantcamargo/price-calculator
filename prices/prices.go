50aa4539cf97882a75397f6e42c2347ca3c70af7
Tauan Camargo <lukyhenson@gmail.com>
2024-05-26 06:01:05 -0300

    refactor: update price package

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -15,36 +14,17 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
+  lines, err := filemanager.ReadLines("prices.txt")
 
-	if err != nil {
-		fmt.Println("Error opening file")
-		fmt.Println(err)
-		return
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-
-	if err != nil {
-		fmt.Println("Error reading file")
-		fmt.Println(err)
-		file.Close()
-		return
-	}
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
 	prices, err := conversion.StringsToFloat(lines)
   
   if err != nil {
     fmt.Println(err)
-    file.Close()
     return
   }
 
