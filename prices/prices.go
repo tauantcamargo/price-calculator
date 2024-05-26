a93821a02e1e16327525f4d095950f462ea74ce4
Tauan Camargo <lukyhenson@gmail.com>
2024-05-26 06:22:49 -0300

    refactor: update prices to have filemanager in struct

--- prices/prices.go
+++ prices/prices.go
@@ -8,13 +8,14 @@ import (
 )
 
 type TaxIncludedPriceJob struct {
+	IOManager         filemanager.FileManager
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	lines, err := filemanager.ReadLines("prices.txt")
+	lines, err := job.IOManager.ReadLines()
 
 	if err != nil {
 		fmt.Println(err)
@@ -42,15 +43,16 @@ func (job *TaxIncludedPriceJob) Process() {
 
 	job.TaxIncludedPrices = result
 
-	err := filemanager.WriteJson(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
+	err := job.IOManager.WriteResult(job)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func New(taxRate float64) *TaxIncludedPriceJob {
+func New(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   fm,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
