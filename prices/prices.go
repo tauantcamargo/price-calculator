adad284ce010cb504ef8eb97e540e323b3b1f549
Tauan Camargo <lukyhenson@gmail.com>
2024-05-26 06:09:44 -0300

    feat: consume write func

--- prices/prices.go
+++ prices/prices.go
@@ -10,23 +10,23 @@ import (
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-  lines, err := filemanager.ReadLines("prices.txt")
+	lines, err := filemanager.ReadLines("prices.txt")
 
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	prices, err := conversion.StringsToFloat(lines)
-  
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	job.InputPrices = prices
 }
@@ -40,7 +40,13 @@ func (job *TaxIncludedPriceJob) Process() {
 		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludedPrices = result
+
+	err := filemanager.WriteJson(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func New(taxRate float64) *TaxIncludedPriceJob {
