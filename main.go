33657376727c26217426548981f3b7171b59923c
Tauan Camargo <lukyhenson@gmail.com>
2024-05-26 21:01:44 -0300

    feat: add select to handle both error or success

--- main.go
+++ main.go
@@ -10,15 +10,33 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
 	for index, taxRate := range taxRates {
-    doneChans[index] = make(chan bool)
+		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		priceJob := prices.New(fm, taxRate)
-		go priceJob.Process(doneChans[index])
+		go priceJob.Process(doneChans[index], errorChans[index])
 	}
 
-	for _, doneChan := range doneChans {
-		<-doneChan
+	for index := range taxRates {
+		select {
+		case err := <-errorChans[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChans[index]:
+			fmt.Println("Done")
+		}
 	}
+
+	// for _, errorChan := range errorChans {
+	// 	err := <-errorChan
+	// 	fmt.Println(err)
+	// }
+
+	// for _, doneChan := range doneChans {
+	// 	<-doneChan
+	// }
 }
